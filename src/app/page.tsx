e0ec59e43a2afb5c3b6e91835a29045c230d6ce5
luizreboucas <primoneto@gmail.com>
2023-06-26 13:28:25 -0300

    envio pós merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,11 +6,49 @@ import {
   Button,
   Typography,
 } from "@material-tailwind/react";
+import BgImage from '../../public/6229517.jpg'
+import Image from "next/image";
+import { useState } from 'react'
+import axios from 'axios'
+
+interface IUSer{
+  email?: string,
+  senha?: string
+}
+
+const URI = 'http://localhost:3500'
 
 export default function Home() {
+
+  const [user,setUser] = useState<IUSer>({
+    email: '',
+    senha: ''
+  })
+  const login = async() => {
+    try {
+      console.log(user) 
+      const response = await axios.post(`${URI}/login`, user)
+      console.log(response)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
-    <div className="flex flex-col items-center justify-center">
-        <div className="mb-10  w-80 max-w-screen-lg sm:w-96">
+    <div className="flex items-center  h-screen w-screen">
+      <div className={`md:h-screen md:w-1/2 xl:h-screen xl:w-2/3 md:flex hidden relative`}>
+        
+        <Image 
+        src={BgImage}
+        alt="imagem de gestão financeira"
+        className="w-screen"
+        fill={true}
+        />
+        
+      
+      </div>
+      
+      <div className="md:w-1/2 w-screen  xl:w-1/3  flex flex-col items-center">
+      <div className="mb-10  w-80 max-w-screen-lg sm:w-96">
           <h1 className="text-4xl font-extrabold text-blue-700  mt-10">QuikPocket</h1>
           <h2 className="text-xl mt-16 text-gray-800">Seja Bem-Vindo ao QuikPocket</h2>
         </div>  
@@ -24,11 +62,14 @@ export default function Home() {
       </Typography>
       <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
         <div className="mb-4 flex flex-col gap-6">
-          <Input size="lg" label="Email" />
-          <Input type="password" size="lg" label="Password" />
+          <Input size="lg" label="Email" onChange={(e)=> setUser({...user, email: e.target.value})}/>
+          <Input type="password" size="lg" label="Password" onChange={(e)=> setUser({...user,senha: e.target.value})}/>
         </div>
         
-        <Button className="mt-6" fullWidth>
+        <Button 
+          className="mt-6" 
+          fullWidth
+          onClick={()=>login()}>
           Entrar
         </Button>
         <Typography color="gray" className="mt-4 text-center font-normal">
@@ -40,9 +81,13 @@ export default function Home() {
             Cadastrar
           </a>
         </Typography>
+        <div>{user.email}</div>
+        <div>{user.senha}</div>
       </form>
     </Card>
 
+      </div>
+      
        
       </div>
     
